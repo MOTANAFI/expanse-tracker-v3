50bbb0cc26948bc3059ec090bd5ed5325b1d5e7d
LONEWOLF143 <twaha2030@gmail.com>
2024-06-06 15:00:42 +0300

    database connnection

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const register = require("./modules/users/controllers/register");
+const mongoose = require("mongoose");
 
+require("dotenv").config();
 
 const app = express();
 
-app.use(express.json())
+mongoose
+  .connect(process.env.MONGODB_CONNECTION, {})
+  .then(() => {
+    console.log("Database connected succesfully");
+  })
+  .catch(() => {
+    console.log("Database connectin failed");
+  });
+
+  require("./models/users.model")
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        status: "success",
-        message: "app running successfully"
-    })
-})
-app.post("/api/register", register)
+  res.status(200).json({
+    status: "success",
+    message: "app running successfully",
+  });
+});
+app.post("/api/register", register);
 
-
-app.listen(8000, () => console.log("Server running succesfully"))
+app.listen(8000, () => console.log("Server running succesfully"));
